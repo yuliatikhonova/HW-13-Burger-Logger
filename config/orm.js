56f127d4a37fb36b7cae14edcd5cce80442db3be
yulia tikhonova <yuliatikhonova@outlook.com>
2020-06-27 21:14:13 -0700

    wrote out the base for the orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,43 @@
 const connection = require("../config/connection.js");
 
 const orm = {
-  
+    selectAll: function (tableInput, cb) {//check to make sure it works
+        const queryString = "SELECT * FROM ??;";
+        connection.query(queryString, [tableInput], function (err, result) {
+            if (err) {
+                throw err;
+            };
+            cb(result);
+        });
+    },
+
+    insertOne: function (table, cols, vals, cb) {
+        const queryString = "";
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            };
+
+            cb(result);
+        });
+    },
+
+
+    updateOne: function (table, objColVals, condition, cb) {
+        const queryString = "";
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            };
+
+            cb(result);
+        });
+    }
+
 };
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
-
+INSERT INTO burgers (burger_name) VALUES ('Salmon burger');
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
 
