f2b9b93e7975d4068c4595b1339b207aa2fc169c
yulia tikhonova <yuliatikhonova@outlook.com>
2020-06-29 15:24:27 -0700

    finish setting up burger.js and or.js

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,30 @@
 const connection = require("../config/connection.js");
 
+function objToSql(ob) {
+    const arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+      const value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  };
+
 const orm = {
     selectAll: function (tableInput, cb) {//check to make sure it works
-        const queryString = "SELECT * FROM ??;";
+        let queryString = "SELECT * FROM ??;";
         connection.query(queryString, [tableInput], function (err, result) {
             if (err) {
                 throw err;
@@ -12,10 +34,10 @@ const orm = {
     },
 
     insertOne: function (table, cols, vals, cb) {
-        const queryString = "";
+        let queryString = "INSERT INTO ?? ?? VALUES ?;";
         console.log(queryString);
 
-        connection.query(queryString, vals, function (err, result) {
+        connection.query(queryString, [table, cols, vals], function (err, result) {
             if (err) {
                 throw err;
             };
@@ -26,10 +48,10 @@ const orm = {
 
 
     updateOne: function (table, objColVals, condition, cb) {
-        const queryString = "";
+        let queryString = "UPDATE ?? SET ?? WHERE ?";
         console.log(queryString);
 
-        connection.query(queryString, function (err, result) {
+        connection.query(queryString,[table, objToSql(objColVals), condition], function (err, result) {
             if (err) {
                 throw err;
             };
